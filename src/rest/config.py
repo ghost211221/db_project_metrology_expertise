4af72d04ccf7e08f5232af4e0618513250edf7cf
Ilya Solovyev <solovyev.21@yandex.ru>
2020-07-25 13:28:14 +0300

    new file storage path

--- src/rest/config.py
+++ src/rest/config.py
@@ -1,4 +1,6 @@
+import os
 
+ROOTDIR = os.path.dirname(os.path.abspath(__file__))
 
-FILE_STORAGE_PATH = '../../file_storage'
+FILE_STORAGE_PATH = f'{ROOTDIR}/../../file_storage'
 ALLOWED_EXTENSIONS = {'docx'}
